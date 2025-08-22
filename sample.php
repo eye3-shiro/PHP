6e8d68d8c2fab4fe25218d25df76723141b65b7d
eye3-shiro <a.1627.work@gmail.com>
2025-08-22 18:38:32 +0900

    php講座３
    配列$array

--- sample.php
+++ sample.php
@@ -1,11 +1,15 @@
 <?php
 $a = "Hello!";
 echo $a;
-?>
-
-<?php
 $age = 20;
 $test = "テスト";
 echo $age;
 echo $test;
+
+$array  = [1,2,3];
+//配列の宣言
+echo $array[0];
+//結果・・・１が出力される
+$array2 = array(2);
+//結果・・・２が出力される
 ?>
